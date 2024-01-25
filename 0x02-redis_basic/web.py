22ac7638189914998e8692f93f7007a3fdba60da
AflaxCade <aflah.cade@gmail.com>
2024-01-25 04:45:26 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,6 +13,7 @@ def get_page(url: str) -> str:
     if not client.get("count:{}".format(url)):
         client.set("count:{}".format(url), 1)
         client.setex("result:{}".format(url), 10, result)
+        return "0"
     else:
         client.incr("count:{}".format(url), 1)
     return result
